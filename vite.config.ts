0187bcd03098756be46b090d3057a7ede9c527ac
Travis Delly <tdellytech@gmail.com>
2025-12-29 20:45:19 -0700

    oop

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import viteReact from '@vitejs/plugin-react';
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
     port: 8490,
   },
   plugins: [
+    tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    nitroV2Plugin({
+      preset: 'vercel',
+    }),
     tanstackStart(),
+    viteReact(),
   ],
 });
